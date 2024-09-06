1f28ebf47f41c53a9ec853641a3f01e0f718d143
Herve Boutonnet <herve@Herves-MacBook-Pro-2.local>
2024-09-06 17:44:58 +0200

    add django env et env variable for debug

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,6 @@
 import os
+import environ
+
 # import dj_database_url
 
 import sentry_sdk
@@ -6,6 +8,8 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 from pathlib import Path
+
+env = environ.Env(DEBUG=(bool, False))
 
 sentry_sdk.init(
     dsn="",
@@ -25,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["p13-082024-latest.onrender.com", "localhost", "127.0.0.1"]
 
